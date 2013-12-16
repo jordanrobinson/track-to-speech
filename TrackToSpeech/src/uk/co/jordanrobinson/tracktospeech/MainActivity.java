1db9f1ba30b291fba76e1b27ab6861000cfc358e
Jordan Robinson <hypahnova@gmail.com>
2013-12-16 12:04:54 +0000

    Hello world for TTS

--- TrackToSpeech/src/uk/co/jordanrobinson/tracktospeech/MainActivity.java
+++ TrackToSpeech/src/uk/co/jordanrobinson/tracktospeech/MainActivity.java
@@ -2,10 +2,14 @@ package uk.co.jordanrobinson.tracktospeech;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnInitListener {
 
+	private TextToSpeech tts;	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,7 +20,15 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		tts = new TextToSpeech(this, this);
 		return true;
+	}
+	
+	@Override
+	public void onInit(int initStatus) {
+		if (initStatus == TextToSpeech.SUCCESS) {
+			tts.speak("Hello world", TextToSpeech.QUEUE_FLUSH, null);
+		}
 	}
 
 }
