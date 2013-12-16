79056686f90532d9a11532f664f4793413b2d229
Jordan Robinson <hypahnova@gmail.com>
2013-12-16 12:29:02 +0000

    Adds initial implementation of playing track retrieval.

--- TrackToSpeech/src/uk/co/jordanrobinson/tracktospeech/MainActivity.java
+++ TrackToSpeech/src/uk/co/jordanrobinson/tracktospeech/MainActivity.java
@@ -2,18 +2,47 @@ package uk.co.jordanrobinson.tracktospeech;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
 import android.view.Menu;
+
 
 public class MainActivity extends Activity implements OnInitListener {
 
-	private TextToSpeech tts;	
+	private TextToSpeech tts;
+	
+	private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+		
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			String command = intent.getStringExtra("command");
+			Log.d("TrTS action output", action + " - " + command);
+			String artist = intent.getStringExtra("artist");
+			String track = intent.getStringExtra("track");
+			Log.d("TrTS track output", artist + " - " + track);
+		}
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		IntentFilter intentFilter = new IntentFilter();
+		
+		intentFilter.addAction("com.android.music.metachanged");
+		intentFilter.addAction("com.android.music.playstatechanged");
+		intentFilter.addAction("com.android.music.playbackcomplete");
+		intentFilter.addAction("com.android.music.queuechanged");
+		
+		registerReceiver(broadcastReceiver, intentFilter);
+		
 	}
 
 	@Override
@@ -29,6 +58,7 @@ public class MainActivity extends Activity implements OnInitListener {
 		if (initStatus == TextToSpeech.SUCCESS) {
 			tts.speak("Hello world", TextToSpeech.QUEUE_FLUSH, null);
 		}
+		
 	}
 
 }
